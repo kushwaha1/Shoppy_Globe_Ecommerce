0c1d16636904a1442228dbe625673a0543562f5b
Sweety <rsweet375@gmail.com>
2025-11-04 14:40:34 +0530

    create store for cart reducer and add store provider in main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.jsx'
+import { Provider } from 'react-redux'
+import store from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}>
-    <App />
-  </RouterProvider>
+  <Provider store={store}>
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </Provider>
+
 )
